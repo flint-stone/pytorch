1edb13f29e80d1ba223eaa7c2effde13b569f86a
Le Xu <sharonxu65@gmail.com>
2020-05-02 02:34:24 -0500

    print operator names [bug fix]

--- aten/src/ATen/core/dispatch/DispatcherOperatorNames.h
+++ aten/src/ATen/core/dispatch/DispatcherOperatorNames.h
@@ -10,11 +10,29 @@ namespace c10 {
 		DispatcherOperatorNames();
 		DispatcherOperatorNames(DispatcherOperatorNames const& copy);
 		DispatcherOperatorNames& operator=(DispatcherOperatorNames const & copy);
+		std::list<std::string> list;
 	public:
-		static std::list<std::string> list;
 		static DispatcherOperatorNames& singleton(){
 			static DispatcherOperatorNames instance;
 			return instance;
 		}
+
+		void append(std::string name){
+			list.emplace_back(name);
+		}
+
+		std::string readNames(){
+			std::string list_of_names = "List of names: ";
+			for(auto  op_name : DispatcherOperatorNames::singleton().list){
+				list_of_names+= op_name ;
+				list_of_names+= ",";
+			}
+			return
+		}
+
+		int size(){
+			return list.size();
+		}
+
 	};
 }
